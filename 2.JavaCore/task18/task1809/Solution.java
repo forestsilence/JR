73ae270e132040868a0892c3a71f6d7c2da914ad
forestsilence <whitepeak@mail.ru>
2021-04-07 23:32:47 +0400

    Second commit

--- 2.JavaCore/task18/task1809/Solution.java
+++ 2.JavaCore/task18/task1809/Solution.java
@@ -2,13 +2,22 @@ package com.javarush.task.task18.task1809;
 
 /*
 Реверс файла
+Считать с консоли 2 имени файла: файл1, файл2.
+Записать в файл2 все байты из файл1, но в обратном порядке.
+Закрыть потоки.
+
+Требования:
+1. Программа должна два раза считать имена файлов с консоли.
+2. Для чтения из файла используй поток FileInputStream, для записи в файл - FileOutputStream
+3. Во второй файл нужно записать все байты из первого в обратном порядке.
+4. Потоки FileInputStream и FileOutputStream должны быть закрыты.
 */
 
 import java.io.*;
 
 public class Solution {
     public static void main(String[] args) {
-        boolean test = true; // TODO must be false
+        boolean test = false; // TODO must be false
         String fileNameIn = "";
         String fileNameOut = "";
         long start = 0;
